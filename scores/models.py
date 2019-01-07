fdb14981ddae296145ea46e66a9ee02689cbb02e
vedantc98 <vedantc98@gmail.com>
2019-01-07 21:20:06 +0530

    Added models for Match and commentary

--- scores/models.py
+++ scores/models.py
@@ -2,3 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+class Match(models.Model):
+	sport_choices = (
+			('Cricket', 'Cricket'),
+			('Football', 'Football'),
+			)
+	team_choices = (
+			('Umiam', 'Umiam'),
+			('Barak', 'Barak'),
+			)
+
+	match_time = models.DateTimeField(auto_now=False)
+	sport = models.CharField(choices=sport_choices)
+	team1 = models.CharField(choices=team_choices)
+	team2 = models.CharField(choices=team_choices)
+
+	location = models.CharField(max_length=20)
+
+class Comment(models.Model):
+
+	match = models.ForeignKey(Match)
+
+	timestamp = models.DateTimeField(auto_now=True)
+	comment_text = models.TextField(max_length=1000)
